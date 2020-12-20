8dce554f521af533544cb7a7c93e90e83e989e2e
Akhil Ashokan <akhilashokan1996@gmail.com>
2020-12-20 22:24:53 +0530

    stripe added

--- product.php
+++ product.php
@@ -2,13 +2,15 @@
     require_once "connect.php";
     $idr=$_GET['id'];
     //echo $idr;
-    $sql = "SELECT * FROM `products` WHERE 1";
-    $product=$sqlCon->query($sql);
-    echo($product);
+    $sql = "SELECT * FROM `products` WHERE id ='$idr'";
+    $result=$sqlCon->query($sql);
+    $followingdata = $result->fetch_assoc();
+   // echo $followingdata['name'] ;
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" href="res/style.css">
 <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
       rel="stylesheet"
@@ -20,12 +22,22 @@
     <title>product</title>
 </head>
 <body>
-    <div class="container">
+    <div class="container product_PgCont">
         <div class="row">
-            <div class="col-6">
-                <img src="<?php  ?> " alt="">
+            <div class="col-4">
+                <img src="<?php echo $followingdata['img']  ?> " alt="">
+            </div>
+            <div class="col-8">
+                <h2> <?php  echo$followingdata['name'] ?>  </h2>
+                <h3> rs: <?php  echo$followingdata['cost'] ?> </h3>
+                <input type="button" value="submit" class="btn btn-dark btn-large" onClick= buy() >
             </div>
         </div>
     </div>
 </body>
 </html>
+<script>
+      function buy(){
+        document.location.href="checkout.html";
+      }
+    </script>
